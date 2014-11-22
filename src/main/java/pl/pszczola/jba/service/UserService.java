6f16d3f3a0eb930eca755291a916233b050659be
Anomander47 <m.pszczolinski92@gmail.com>
2014-11-22 15:43:22 +0100

    Status 26/51

--- src/main/java/pl/pszczola/jba/service/UserService.java
+++ src/main/java/pl/pszczola/jba/service/UserService.java
@@ -1,5 +1,6 @@
 package pl.pszczola.jba.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,13 +8,16 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import pl.pszczola.jba.entity.Blog;
 import pl.pszczola.jba.entity.Item;
+import pl.pszczola.jba.entity.Role;
 import pl.pszczola.jba.entity.User;
 import pl.pszczola.jba.repository.BlogRepository;
 import pl.pszczola.jba.repository.ItemRepository;
+import pl.pszczola.jba.repository.RoleRepository;
 import pl.pszczola.jba.repository.UserRepository;
 
 @Service
@@ -22,6 +26,9 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Autowired
 	private BlogRepository blogRepository;
@@ -52,6 +59,15 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		user.setEnabled(true);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		user.setPassword(encoder.encode(user.getPassword()));
+		
+		
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(roleRepository.findByName("ROLE_USER"));
+		user.setRoles(roles);
+		
 		userRepository.save(user);
 		
 	}
